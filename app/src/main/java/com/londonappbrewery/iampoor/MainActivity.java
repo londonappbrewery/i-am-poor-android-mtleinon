6a381ebe24eb34a8b4cfd35d83f57f1f18957fe6
mtleinon <mika.t.leinonen@gmail.com>
2017-10-26 16:49:25 +0300

    Challenge completed.

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -1,8 +1,8 @@
 package com.londonappbrewery.iampoor;
 
 import android.app.Activity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
